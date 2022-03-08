ea096fa33a9f07cce812ce55266c5f940c57879a
Kristina-VCyber <hatkova@inbox.ru>
2022-03-08 19:03:32 +0300

    microtask02-additional task: add: topCars array, type TopCarsType use: map for table to render objFromTopCarsArr manufacturer and model

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import {NewComponent} from "./components/NewComponent";
 
 
 
-const students = [
-    {id: 1, name: "Tina", age: 29},
-    {id: 2, name: "John", age: 25},
-    {id: 3, name: "Harry", age: 34},
-];
+// const students = [
+//     {id: 1, name: "Tina", age: 29},
+//     {id: 2, name: "John", age: 25},
+//     {id: 3, name: "Harry", age: 34},
+// ];
 
-// const topCars = [
-//     {manufacturer: "BMW", model: "m5cs"},
-//     {manufacturer: "Mercedes", model: "e63s"},
-//     {manufacturer: "Audi", model: "rs6"},
-// ]
+const topCars = [
+    {manufacturer: "BMW", model: "m5cs"},
+    {manufacturer: "Mercedes", model: "e63s"},
+    {manufacturer: "Audi", model: "rs6"},
+]
 
 function App() {
     return (
         <>
-            <NewComponent students={students} />
+            <NewComponent topCars={topCars} />
         </>
     );
 }
--- src/components/NewComponent.tsx
+++ src/components/NewComponent.tsx
@@ -1,41 +1,66 @@
 
+// type NewComponentType = {
+//     students: Array<StudentType>
+//     // students:StudentType[]
+// }
+// type StudentType = {
+//     id:number,
+//     name:string,
+//     age:number
+// }
+
+
 type NewComponentType = {
-    students: Array<StudentType>
-    // students:StudentType[]
-}
-type StudentType = {
-    id:number,
-    name:string,
-    age:number
+    topCars: TopCarsType[]
 }
 
-//
-// type NewComponentType = {
-//     topCars: TopCarsType[]
-// }
-//
-// type TopCarsType= {
-//     manufacturer: string
-//     model:string
-// }
+type TopCarsType= {
+    manufacturer: string
+    model:string
+}
 
 
 export const NewComponent = (props:NewComponentType) => {
 
 
     return (
-       <ul>
-           {props.students.map((objFromStudentArr, index) => {
+        <table>
+            <tr>
+                <th>manufacturer</th>
+                <th>model</th>
+            </tr>
 
-               return (
-                   // <li key = {objFromStudentArr.id}>{objFromStudentArr.name + " " + objFromStudentArr.age}</li>
-                   <li key = {objFromStudentArr.id}>
-                       <span>{objFromStudentArr.name}</span>
-                       <span> age: {objFromStudentArr.age}</span>
-                   </li>
+            {props.topCars.map((objFromTopCarsArr, index) => {
+                return (
+                <tr>
+                <td key={index}>{objFromTopCarsArr.manufacturer}</td>
+                <td key={index}>{objFromTopCarsArr.model}</td>
+                </tr>
+                )
+            })}
+        </table>
 
-               )
-           })}
-       </ul>
+
+
+
+
+
+
+
+
+
+       // <ul>
+       //     {props.students.map((objFromStudentArr, index) => {
+       //
+       //         return (
+       //             // <li key = {objFromStudentArr.id}>{objFromStudentArr.name + " " + objFromStudentArr.age}</li>
+       //             <li key = {objFromStudentArr.id}>
+       //                 <span>{objFromStudentArr.name}</span>
+       //                 <span> age: {objFromStudentArr.age}</span>
+       //             </li>
+       //
+       //         )
+       //     })}
+       // </ul>
     )
 }
