2a4a0296c6b39abe000c9db1f301663093f298ec
Kristina-VCyber <hatkova@inbox.ru>
2022-03-09 23:29:16 +0300

    microtask03 create: function ButtonLastExpression add: StupidButton

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,21 @@ function App() {
     //     console.log(name)
     // }
 
-    const Button1Foo = (subscriber:string, age:number, address:string) => {
-        console.log(subscriber,age, address)
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
     }
-    const Button2Foo = (subscriber:string, age:number) => {
-        console.log(subscriber,age)
+    const Button2Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
     }
-
+    const ButtonLastExpression = () => {
+        console.log("I'm stupid button")
+    }
 
     return (
         <div className={"App"}>
-            <Button name={"MyYouTubeChannel-1"} callBack={() => Button1Foo ("I'm Dobbi", 21, "live in Tokyo")}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={() => Button2Foo ("I'm Han Solo", 42)}/>
+            <Button name={"MyYouTubeChannel-1"} callBack={() => Button1Foo("I'm Dobbi", 21, "live in Tokyo")}/>
+            <Button name={"MyYouTubeChannel-2"} callBack={() => Button2Foo("I'm Han Solo", 42)}/>
+            <Button name={"StupidButton"} callBack={ButtonLastExpression}/>
         </div>
     );
 }
