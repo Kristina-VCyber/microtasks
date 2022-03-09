951f60ce46e4559d6a0e119ef75a307800749b48
Kristina-VCyber <hatkova@inbox.ru>
2022-03-09 22:52:47 +0300

    microtask03 add: function myFirstSubscriber, mySecond Subscriber add: button 1,2 add: function firstBtn,secondBtn

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // const myFirstSubscriber = (event: React.MouseEvent<HTMLButtonElement>)  => {
+    //     console.log("Hello, I'm Dobbi")
+    // }
+    // const mySecondSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello, I'm Ron")
+    // }
+
+    // const onClickHandler = (name:string) => {
+    //     console.log(name)
+    // }
+
+
+    const firstBtn = () => {
+        console.log(100200)
+    }
+
+    const secondBtn = (value:number) => {
+        console.log(value)
+    }
   return (
     <div className="App">
-      <button onClick={() => {}}>MyYouTubeChanel</button>
+      {/*<button onClick={(event) => {console.log("Hello")}}>MyYouTubeChanel-1</button>*/}
+      {/*<button onClick={ (event) => onClickHandler("Dobbi")}>  MyYouTubeChanel-1</button>*/}
+      {/*<button onClick={(event) => onClickHandler("Ron")}>MyYouTubeChanel-2</button>*/}
+
+
+<button onClick={firstBtn}>1</button>
+<button onClick={(event)=>secondBtn(100200)}>2</button>
+
     </div>
   );
 }
