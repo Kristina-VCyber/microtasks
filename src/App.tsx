dccdf1a44b053d1cb8e36690c9d1e2c7df5fc1bd
Kristina-VCyber <hatkova@inbox.ru>
2022-03-13 13:36:05 +0300

    microtasks 06 create:InputTraining component, FullInputComponent add: addMessage function, forward it to FullInput component add: onChangeInputHandler function and onClickBtnHandler function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {Button} from "./components/Button";
+
+import {InputTraining} from "./components/InputTraining";
 
 function App() {
     // const myFirstSubscriber = (event: React.MouseEvent<HTMLButtonElement>)  => {
@@ -14,21 +15,25 @@ function App() {
     //     console.log(name)
     // }
 
-    const Button1Foo = (subscriber: string, age: number, address: string) => {
-        console.log(subscriber, age, address)
-    }
-    const Button2Foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-    const ButtonLastExpression = () => {
-        console.log("I'm stupid button")
-    }
+    // const Button1Foo = (subscriber: string, age: number, address: string) => {
+    //     console.log(subscriber, age, address)
+    // }
+    // const Button2Foo = (subscriber: string, age: number) => {
+    //     console.log(subscriber, age)
+    // }
+    // const ButtonLastExpression = () => {
+    //     console.log("I'm stupid button")
+    // }
 
     return (
         <div className={"App"}>
-            <Button name={"MyYouTubeChannel-1"} callBack={() => Button1Foo("I'm Dobbi", 21, "live in Tokyo")}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={() => Button2Foo("I'm Han Solo", 42)}/>
-            <Button name={"StupidButton"} callBack={ButtonLastExpression}/>
+            {/*<Button name={"MyYouTubeChannel-1"} callBack={() => Button1Foo("I'm Dobbi", 21, "live in Tokyo")}/>*/}
+            {/*<Button name={"MyYouTubeChannel-2"} callBack={() => Button2Foo("I'm Han Solo", 42)}/>*/}
+            {/*<Button name={"StupidButton"} callBack={ButtonLastExpression}/>*/}
+            {/*<UseStateTraining/>*/}
+            {/*<FilterTraining/>*/}
+            <InputTraining message={"How are you?"}/>
+
         </div>
     );
 }
---
+++ src/components/InputTraining.tsx
@@ -0,0 +1,38 @@
+import {useState} from "react";
+import {FullInput} from "./componentsForInput/FullInput";
+
+type Props = {
+    message: string
+}
+
+export const InputTraining = (props: Props) => {
+
+    let [message, setMessage] = useState([
+        {message: "message1"},
+        {message: "message2"},
+        {message: "message3"}
+    ])
+
+    const addMessage = (inputTitle: string) => {
+        console.log(inputTitle)
+    }
+
+    return (
+        <div className={"InputTraining"}>
+            {/*<div>*/}
+            {/*    <input type="text"/>*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
+            <FullInput addMessage={addMessage}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+
+        </div>
+
+    )
+
+
+}
